5291d6140d2d1e93579ce8fb11c6c60b0fd80618
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2025-02-27 19:52:28 +0530

    add to git

--- Arrays/solution.java
+++ Arrays/solution.java
@@ -1,22 +1,41 @@
+// class solution {
+//     public static void main(String[] args) {
+//         int[] nums = {0,0,0,1,1,1,2,2,3,3,4};
+//         int ans = removeDuplicates(nums);
+//         System.out.println(ans);
+// //        System.out.println(nums);
+                
+//             }
+//             public static int removeDuplicates(int[] nums) {
+//             if (nums.length==0){
+//                 return 0;
+//             }
+//             int uniquenum = 1;
+//             for(int i = 1;i<nums.length;i++){
+//                 if(nums[i-1] != nums[i]){
+//                     nums[uniquenum] = nums[i];
+//                 uniquenum ++;
+//             }
+//         }
+//         return uniquenum;
+//     }
+// }
+
 class solution {
     public static void main(String[] args) {
-        int[] nums = {0,0,0,1,1,1,2,2,3,3,4};
-        int ans = removeDuplicates(nums);
-        System.out.println(ans);
-//        System.out.println(nums);
-                
-            }
-            public static int removeDuplicates(int[] nums) {
-            if (nums.length==0){
-                return 0;
-            }
-            int uniquenum = 1;
-            for(int i = 1;i<nums.length;i++){
-                if(nums[i-1] != nums[i]){
-                    nums[uniquenum] = nums[i];
-                uniquenum ++;
+        int[] nums = {0,1,0,3,10};
+        int[] ans = moveZeroes(nums);
+        System.out.println(nums);
+    }
+        public static int[]  moveZeroes(int[] nums) {
+        int pnt = 1;
+        for (int i = 0; i < nums.length-1; i++) {
+            if(nums[i]==0){
+                int temp = nums[pnt];
+                nums[pnt] = nums[pnt - 1];
+                nums[pnt + 1] = temp;
             }
         }
-        return uniquenum;
+                return nums;
     }
 }
